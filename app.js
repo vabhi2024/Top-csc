79ee865e4b4fc6ba203cf94d936f90c3b54d0192
vabhi2024 <vabhi2024@gmail.com>
2026-02-01 22:03:53 +0530

    app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    document.getElementById("pwa-splash").style.display = "none";
+    document.getElementById("app").style.display = "block";
+  }, 1500);
+});
